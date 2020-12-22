a3d9ffc86ca2169e7fd19913eb3ead082178d42c
shipeng.xu <shipeng.xu@salesforce.com>
2020-12-22 15:09:59 -0800

    added new problem.

--- src/main/java/serialization/Person.java
+++ src/main/java/serialization/Person.java
@@ -11,6 +11,13 @@ public class Person implements Serializable {
      *
      *  You'll get a popover that says "Randomly Change serialVersionUID Initializer". Just hit enter, and it'll populate that space with a random long.
      *
+     *  Simply put, the serialVersionUID is a unique identifier for Serializable class.
+     *  This is used during the deserialization of an object, to ensure that a loaded class is compatible with teh serialized object.
+     *  if no matching class is found, an InvalidClassException is thrown.
+     *
+     *  Because of this sort of unwanted incompatibility, it's always a good idea to declare a serialVersionUID in Serializable classes.
+     *
+     *
      */
     private static final long serialVersionUID = 3803934470915584742L;
 
